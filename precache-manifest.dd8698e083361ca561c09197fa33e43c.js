f9a6d1111b49198dc82ba2c62c78f0e44a8fd536
Riaz Khan <goriazmte@gmail.com>
2021-04-07 21:30:39 +0600

    Updates

---
+++ precache-manifest.dd8698e083361ca561c09197fa33e43c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b41320a3f09aba669b0d64d3cbd72a33",
+    "url": "/react_portfolio_1/index.html"
+  },
+  {
+    "revision": "d1a6f6febc4f4ea496bf",
+    "url": "/react_portfolio_1/static/css/main.ad466257.chunk.css"
+  },
+  {
+    "revision": "b59cb2ee616408d5f0b0",
+    "url": "/react_portfolio_1/static/js/2.baeeef23.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/react_portfolio_1/static/js/2.baeeef23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1a6f6febc4f4ea496bf",
+    "url": "/react_portfolio_1/static/js/main.1234e93f.chunk.js"
+  },
+  {
+    "revision": "88c6831ae78b2b69f2c7",
+    "url": "/react_portfolio_1/static/js/runtime-main.c331dd99.js"
+  },
+  {
+    "revision": "0685327bad5eeb03ebda59c4e5b19ffc",
+    "url": "/react_portfolio_1/static/media/COVID.0685327b.jpg"
+  },
+  {
+    "revision": "f5711eaa4996e1483f221e7c57cb7905",
+    "url": "/react_portfolio_1/static/media/algo.f5711eaa.svg"
+  },
+  {
+    "revision": "63f68ccc85cc2ab7da913353c08cccaf",
+    "url": "/react_portfolio_1/static/media/api.63f68ccc.svg"
+  },
+  {
+    "revision": "a42040d5c68d4d3fed9b27878d2fcca5",
+    "url": "/react_portfolio_1/static/media/backend.a42040d5.svg"
+  },
+  {
+    "revision": "ca042db5d0a17f1f6ee36ea90fa2f3a4",
+    "url": "/react_portfolio_1/static/media/computer.ca042db5.svg"
+  },
+  {
+    "revision": "0a27bbff6aa4dfb3bf8606c2fba69fd6",
+    "url": "/react_portfolio_1/static/media/facebook.0a27bbff.svg"
+  },
+  {
+    "revision": "cbc38ef18c5a6ecdc0ace14b23e84380",
+    "url": "/react_portfolio_1/static/media/github.cbc38ef1.svg"
+  },
+  {
+    "revision": "d08bce0dcb53ad1ef20b0cae18977145",
+    "url": "/react_portfolio_1/static/media/instagram.d08bce0d.svg"
+  },
+  {
+    "revision": "626e64ee506bc531adb9e1071fcb3d73",
+    "url": "/react_portfolio_1/static/media/mypic.626e64ee.jpg"
+  },
+  {
+    "revision": "64c56c72135ba0b4ddf8da186182b723",
+    "url": "/react_portfolio_1/static/media/pin.64c56c72.svg"
+  },
+  {
+    "revision": "b5fac2be7a6a8edc808f550a24217391",
+    "url": "/react_portfolio_1/static/media/puzzle.b5fac2be.svg"
+  },
+  {
+    "revision": "26c488dd1be55304e547e30598fb5ca8",
+    "url": "/react_portfolio_1/static/media/python.26c488dd.svg"
+  },
+  {
+    "revision": "2e01c74fe6fcd933e0e9f338e56c0922",
+    "url": "/react_portfolio_1/static/media/react.2e01c74f.svg"
+  },
+  {
+    "revision": "1464053c658e62318c689e56c4f1643c",
+    "url": "/react_portfolio_1/static/media/repair.1464053c.svg"
+  },
+  {
+    "revision": "fb9a6c70bd23d842ef9119ca4e6704ed",
+    "url": "/react_portfolio_1/static/media/resume_riaz.fb9a6c70.pdf"
+  },
+  {
+    "revision": "4a04f9f0dd3f59df96497cc7c3021550",
+    "url": "/react_portfolio_1/static/media/tie.4a04f9f0.svg"
+  }
+]);
